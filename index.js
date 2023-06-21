2364b370ae8252ea7c8aa0353fd7834ecca23d0e
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-21 14:19:56 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\s'-]/g, '');
+  }
+
+  static titleize(sentence) {
+    const lowerCaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+    const words = sentence.toLowerCase().split(" ");
+
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !lowerCaseWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return capitalizedWords.join(' ');
+  }
 }
